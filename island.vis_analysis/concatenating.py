a9447d2dbfc9373d64854ad50bcb25a2e1d35370
BahneTP <bahne.thielpeters@gmail.com>
2025-07-03 08:07:01 +0000

    restructured the repository

---
+++ island.vis_analysis/concatenating.py
@@ -0,0 +1,47 @@
+import os
+import xarray as xr
+
+folders = ["reanalysis"]
+
+def combine(path):
+    datasets = []
+    folder_path = os.path.abspath(path)
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".nc"):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                ds = xr.open_dataset(file_path)
+
+                # Delete bottomT if existing.
+                if "bottomT" in ds:
+                    ds = ds.drop_vars("bottomT")
+
+                datasets.append(ds)
+
+            except Exception as e:
+                print(f"Fehler bei {filename}: {e}")
+    return xr.merge(datasets, combine_attrs="override")
+
+# Now merging.
+reanalysis = combine("reanalysis")
+reanalysis = reanalysis.sel(time=slice(None, "2022-12-31"))
+print(reanalysis.thetao.values.shape)
+
+forecasting = combine("forecasting")
+print(forecasting.thetao.values.shape)
+
+combined = xr.concat([reanalysis, forecasting], dim="time", combine_attrs="override")
+print(combined.thetao.values.shape)
+
+
+# Now we need to ensure that we use the right missing_value's and _FillValue's for the saving.
+for var in combined.data_vars:
+    v = combined[var]
+    v.encoding['_FillValue'] = 1.0e+20
+    if 'missing_value' in v.encoding:
+        del v.encoding['missing_value']
+    v.attrs['missing_value'] = 1.0e+20  # optional, CF-Doku
+
+combined.to_netcdf("combined.nc", format="NETCDF4")
+
